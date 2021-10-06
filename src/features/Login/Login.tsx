74f465466a9a77817a822b1224b36585f69caf04
VladAlmazov <vladosa113@gmail.com>
2021-10-06 14:20:39 +0300

    Initial commit

---
+++ src/features/Login/Login.tsx
@@ -0,0 +1,97 @@
+import React from 'react'
+import {Checkbox, FormControl, FormControlLabel, FormGroup, FormLabel, TextField, Button, Grid} from '@material-ui/core'
+import {useFormik} from 'formik';
+import {useDispatch, useSelector} from 'react-redux';
+import { loginTC } from './auth-reducer';
+import {AppRootStateType} from '../../app/store';
+import {Redirect} from 'react-router-dom';
+import style from './Login.module.css'
+
+type FormikErrorType = {
+    email?: string
+    password?: string
+    rememberMe?: boolean
+}
+
+
+export const Login = () => {
+    const dispatch = useDispatch()
+
+    const isLoggedIn = useSelector<AppRootStateType, boolean>(state => state.auth.isLoggedIn);
+
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+            rememberMe: false
+        },
+        validate: (values) => {
+            const errors: FormikErrorType = {};
+            if (!values.email) {
+                errors.email = 'Required';
+            } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+                errors.email = 'Invalid email address';
+            }
+
+            if (!values.password) {
+                errors.password = 'Required';
+            } else if (values.password.length < 3) {
+                errors.password = 'Must be 3 characters or more';
+            }
+
+            return errors;
+        },
+        onSubmit: values => {
+            dispatch(loginTC(values))
+            formik.resetForm();
+        },
+    })
+
+    if (isLoggedIn) {
+        return <Redirect to={'/'}/>
+    }
+
+    return <Grid container justify="center">
+        <Grid item xs={4}>
+            <FormControl className={style.loginForm}>
+                <FormLabel>
+                    <p>To log in get registered
+                        <a href={'https://social-network.samuraijs.com/'}
+                           target={'_blank'}>here
+                        </a>
+                    </p>
+                    <p>or use common test account credentials:</p>
+                    <p>Email: free@samuraijs.com</p>
+                    <p>Password: free</p>
+                </FormLabel>
+                <form onSubmit={formik.handleSubmit}>
+                    <FormGroup>
+                        <TextField
+                            label="Email"
+                            margin="normal"
+                            {...formik.getFieldProps('email')}
+                        />
+                        {formik.touched.email
+                        && formik.errors.email
+                        && <div style={{color: 'red'}}>{formik.errors.email}</div>}
+                        <TextField
+                            type="password"
+                            label="Password"
+                            margin="normal"
+                            {...formik.getFieldProps('password')}
+                        />
+                        {formik.touched.password
+                        && formik.errors.password
+                        && <div style={{color: 'red'}}>{formik.errors.password}</div>}
+                        <FormControlLabel
+                            label={'Remember me'}
+                            control={<Checkbox checked={formik.values.rememberMe}
+                                {...formik.getFieldProps('rememberMe')}/>}
+                        />
+                        <Button type={'submit'} variant={'contained'} color={'primary'}>Login</Button>
+                    </FormGroup>
+                </form>
+            </FormControl>
+        </Grid>
+    </Grid>
+}
